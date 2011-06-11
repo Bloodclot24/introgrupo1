8ab678f2e73d77f2964e180953275b9cfbb9a756
seba.piccoli@gmail.com <seba.piccoli@gmail.com@3c184c17-dadf-03fa-cae5-bfa383f5d530>
2011-06-11 18:08:17 +0000

    ya casiii

--- Scripts/network_interfaces.sh
+++ Scripts/network_interfaces.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 #uso: network_interfaces <router>
 
+if [ "$#" -ne 1 ]
+then
+	echo "ERROR: Cantidad incorrecta de parametros"	
+	echo "Uso: ./network_interfaces <router>"
+	#codigo de retorno 1 para cantidad incorrecta de parametros
+	exit 1
+fi
+
 router=$1
 
 #borro las interfaces previamente definidas
@@ -10,9 +18,18 @@ sudo ifconfig eth2 up
 #cargo las interfaces para este router
 cd ./config/interfaces
 
+if [ ! -r "$1" ]
+then
+	echo "No existe o no puede leerse archivo para el router especificado."
+	#codigo de retorno 2 para archivo inexistente
+	exit 2
+fi
+
 i=0
 for interfaz in `cat "$router"`
 do
-	echo `echo "$interfaz" | sed "s/\(^[0-9,.]*\);\([0-9,.]*$\)/sudo ifconfig eth2:$i \1 netmask \2/"`
-	i=$i+1
+	`echo "$interfaz" | sed "s/\(^[0-9,.]*\);\([0-9,.]*$\)/sudo ifconfig eth2:$i \1 netmask \2/"`
+	let i=$i+1
 done
+
+exit 0
