ac2caacf533985f7a40028fbb0a954b24fff10eb
seba.piccoli@gmail.com <seba.piccoli@gmail.com@3c184c17-dadf-03fa-cae5-bfa383f5d530>
2011-06-11 17:55:12 +0000

    script de carga de interfaces

--- trunk/Scripts/network_interfaces.sh
+++ trunk/Scripts/network_interfaces.sh
@@ -4,15 +4,15 @@
 router=$1
 
 #borro las interfaces previamente definidas
-#ifconfig eth2 down
-#ifconfig eth2 up
+sudo ifconfig eth2 down
+sudo ifconfig eth2 up
 
 #cargo las interfaces para este router
 cd ./config/interfaces
 
-i=1
+i=0
 for interfaz in `cat "$router"`
 do
-	#echo `sed 's/^\(*\);\(*\)$/ifconfig 'eth2.'$i \1 -netmask \2/' $interfaz`
+	echo `echo "$interfaz" | sed "s/\(^[0-9,.]*\);\([0-9,.]*$\)/sudo ifconfig eth2:$i \1 netmask \2/"`
 	i=$i+1
 done
