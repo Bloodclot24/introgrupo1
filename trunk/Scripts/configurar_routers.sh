9d82c0a5b99bafcb75b2a3de51eb50b8729ad63b
gygounet@gmail.com <gygounet@gmail.com@3c184c17-dadf-03fa-cae5-bfa383f5d530>
2011-06-12 14:41:15 +0000

    Modificaciones a los scripts... falta todavia

--- trunk/Scripts/configurar_routers.sh
+++ trunk/Scripts/configurar_routers.sh
@@ -6,7 +6,7 @@
 
 if [$# -ne 1]
 then
-    echo Error, cantidad de parametros invalida...
+    echo "Error, cantidad de parametros invalida"
     exit 1
 fi
 
@@ -19,13 +19,13 @@ service avahi-daemon stop > /dev/null
 
 #Eliminar configuracion de rutas anteriores
 
-echo Eliminando rutas anteriores de la tabla de ruteo
+echo "Eliminando rutas anteriores de la tabla de ruteo"
 ip route flush default
-echo Tabla de ruteo limpia.
+echo "Tabla de ruteo limpia"
 
 #Configurar rutas
 
-echo Configurando las rutas para el router $i
+echo "Configurando las rutas para el router" $i
 old_IFS=$IFS;
 IFS="
 ";
@@ -33,20 +33,20 @@ IFS="
 for i in `sed "s/^\([^;]*\);\([^;]*\);\([^;]*\);\([^;]*\);*$/\1 netmask \2 gw \3 metric \4/" /config/$1`
 do
 	IFS=" ";
-	echo route add -net $i
+	echo "route add -net" $i
 	route add -net $i
 	IFS="
 	";
 done
 
 IFS=$old_IFS;
-echo Rutas configuradas en el router $i
+echo "Rutas configuradas en el router" $i
 
 #Habilitar el ruteo, permitir que las computadoras forwardeen paquetes
 
-echo Habilitando el forwarding de paquetes en el router $i
+echo "Habilitando el forwarding de paquetes en el router" $i
 sysctl -w net.ipv4.ip_forward=1
-echo Forwarding de paquetes habilitado
+echo "Forwarding de paquetes habilitado"
 
 exit 0
 
--- trunk/Scripts/habilitar_servicios.sh
+++ trunk/Scripts/habilitar_servicios.sh
@@ -1,30 +1,39 @@
 #!/bin/bash
 
-#Habilita los Servicios WebServer, FtpServer, TelnetServer dependiendo del parametro con el que se lo invoque
+#Habilita los servicios de  WebServer, FtpServer, TelnetServer dependiendo del parametro con el que se lo invoque
+#En caso de invocarselo sin parametros habilita los servicios de WebServer,FtpServer, TelnetServer.
 
-if [$# -ne 1]
+if [$# -gt 1]
 then
-    echo Error, cantidad de parametros invalida
+    echo "Error, cantidad de parametros invalida"
     exit 1
+else if	[$# -eq 0 ]
+    then
+	echo "Habilitando todos los servidores"
+	service apache2 start
+	service vsftpd start
+	service openbsd/inetd start
+	echo "El WebServer, FtpServer, TelnetServer se encuentran habilitados"
+    fi
 fi
 
 #Habilitando los servicios
 
 if [$1 -eq WebServer]
 then
-    echo Habilitando servidor web
+    echo "Habilitando servidor web"
     service apache2 start
 fi
 
 if [$1 -eq FtpServer]
 then
-    echo Habilitando servidor ftp
+    echo "Habilitando servidor ftp"
     service vsftpd  start
 fi
 
 if [$1 -eq TelnetServer]
 then
-    echo Habilitando servidor telnet
+    echo "Habilitando servidor telnet"
     service openbsd-inetd start
 fi
 
