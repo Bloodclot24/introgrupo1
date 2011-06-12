b5d696b53b94641a4bc8b4b96553674eee3e7cb8
gygounet@gmail.com <gygounet@gmail.com@3c184c17-dadf-03fa-cae5-bfa383f5d530>
2011-06-12 14:49:20 +0000

    Fix espacios? xD

--- trunk/Scripts/habilitar_servicios.sh
+++ trunk/Scripts/habilitar_servicios.sh
@@ -3,35 +3,27 @@
 #Habilita los servicios de  WebServer, FtpServer, TelnetServer dependiendo del parametro con el que se lo invoque
 #En caso de invocarselo sin parametros habilita los servicios de WebServer,FtpServer, TelnetServer.
 
-if [$# -gt 1]
+if [ $# -gt 1 ]
 then
     echo "Error, cantidad de parametros invalida"
     exit 1
-else if	[$# -eq 0 ]
-    then
-	echo "Habilitando todos los servidores"
-	service apache2 start
-	service vsftpd start
-	service openbsd/inetd start
-	echo "El WebServer, FtpServer, TelnetServer se encuentran habilitados"
-    fi
 fi
 
 #Habilitando los servicios
 
-if [$1 -eq WebServer]
+if [ $1 -eq WebServer || $# -eq 0 ]
 then
     echo "Habilitando servidor web"
     service apache2 start
 fi
 
-if [$1 -eq FtpServer]
+if [ $1 -eq FtpServer || $# -eq 0 ]
 then
     echo "Habilitando servidor ftp"
     service vsftpd  start
 fi
 
-if [$1 -eq TelnetServer]
+if [ $1 -eq TelnetServer || $# -eq 0 ]
 then
     echo "Habilitando servidor telnet"
     service openbsd-inetd start
