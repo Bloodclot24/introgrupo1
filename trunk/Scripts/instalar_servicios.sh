de993b0ebb992662422882645ce3189130b87f76
karenroberts16@gmail.com <karenroberts16@gmail.com@3c184c17-dadf-03fa-cae5-bfa383f5d530>
2011-06-13 23:04:15 +0000

    correcciones al script

--- trunk/Scripts/instalar_servicios.sh
+++ trunk/Scripts/instalar_servicios.sh
@@ -4,13 +4,13 @@
 # Se ejecuta en modo root
 
 echo "Instalando servidor web(apache)"
-bash aptitude install apache2
+aptitude install apache2
 
 echo "Instalando servidor ftp"
-bash aptitude install vsftpd
+aptitude install vsftpd
 
 echo "Instalando servidor telnet"
-bash aptitude install openbsd-inetd
+aptitude install openbsd-inetd
 
 echo "Servidores instalados"
 
