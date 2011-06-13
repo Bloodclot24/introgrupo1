043cca40d597cc29ebc022680e44a4387bd3b9bb
karenroberts16@gmail.com <karenroberts16@gmail.com@3c184c17-dadf-03fa-cae5-bfa383f5d530>
2011-06-13 20:56:12 +0000

    correcciones a los scripts, hay lineas comentadassvn st

--- trunk/Scripts/configurar_routers.sh
+++ trunk/Scripts/configurar_routers.sh
@@ -4,7 +4,7 @@
 # Vacia la tabla de ruteo ya existente
 # Habilita el forwarding de paquetes
 
-if [ $# -ne 1 ]
+if [ $# -ne 2 ]
 then
     echo "Error, cantidad de parametros invalida"
     exit 1
@@ -12,10 +12,10 @@ fi
 
 #Detener servicios
 
-service NetworkManager stop > /dev/null
-service network-manager stop > /dev/null
-service apparmor stop > /dev/null
-service avahi-daemon stop > /dev/null
+#service NetworkManager stop > /dev/null
+#service network-manager stop > /dev/null
+#service apparmor stop > /dev/null
+#service avahi-daemon stop > /dev/null
 
 #Eliminar configuracion de rutas anteriores
 
@@ -23,16 +23,19 @@ echo "Eliminando rutas anteriores de la tabla de ruteo"
 ip route flush default
 echo "Tabla de ruteo limpia"
 
+
+bash network_interfaces.sh $1 $2
+
 #Configurar rutas
 
-cd ./config/tablas_de_ruteo
+cd ./config/tablas_de_ruteo/
 
 echo "Configurando las rutas para el router" $i
 old_IFS=$IFS;
 IFS="
 ";
 
-for i in `sed "s/^\([^;]*\);\([^;]*\);\([^;]*\);\([^;]*\);*$/\1 netmask \2 gw \3 metric \4/" /config/$1`
+for i in `sed "s/^\([^;]*\);\([^;]*\);\([^;]*\);\([^;]*\);*$/\1 netmask \2 gw \3 metric \4/" $1`
 do
 	IFS=" ";
 	echo "route add -net" $i
@@ -42,11 +45,11 @@ do
 done
 
 IFS=$old_IFS;
-echo "Rutas configuradas en el router" $i
+echo "Rutas configuradas en el router" $1
 
 #Habilitar el ruteo, permitir que las computadoras forwardeen paquetes
 
-echo "Habilitando el forwarding de paquetes en el router" $i
+echo "Habilitando el forwarding de paquetes en el router" $1
 sysctl -w net.ipv4.ip_forward=1
 echo "Forwarding de paquetes habilitado"
 
